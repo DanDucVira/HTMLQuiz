6bee8c8bf3d5b828cd52986de2b250ef75b48154
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-05-17 10:55:49 -0400

    changes with html and code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const correctAnswers = ['A', 'C', 'B'];
+const correctAnswers = ['A', 'C', 'B', 'D'];
+
 const form = document.querySelector('.quiz-form');
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-    // check answers
+    //check anwsers
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
             score += 25;
